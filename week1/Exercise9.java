a5b5df3c967a454080a01a645baddeb879310ab6
Theang Dyna <theangdyna365@gmail.com>
2022-12-26 18:30:18 +0700

    dyna

--- week1/Exercise9.java
+++ week1/Exercise9.java
@@ -3,14 +3,13 @@ public class Exercise9 {
   public static void main(String[] args) {
     int sum;
 
-    for (int i = 1; i < 10; i++) {
+    for (int i = 1; i < 10000; i++) {
       sum = 0;
 
-      for (int j = 1; j <= i; j++) {
+      for (int j = 1; j < i; j++) {
         if (i % j == 0) {
           sum += j;
         }
-        System.out.println(j + " <= " + i);
       }
 
       if (i == sum) System.out.println(i);
